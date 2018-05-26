a87231fd8f08dc9654ae0cc6a061fd5ec6b190f8
KaitlynDodds <kadodds.dev@kadodds.com>
2018-05-25 18:31:32 -0600

    data scraping working well

--- crawler.js
+++ crawler.js
@@ -6,19 +6,9 @@ function scrape(uri, path = "") {
 
 	// create new crawler 
 	const crawler = new Crawler({
+		rateLimit: 1000,
 		// This will be called for each crawled page
-	    callback : function (err, res, done) {
-			if (err) {
-	            console.log(err);
-	        } else {
-	        	// setup to use jquery
-	            var $ = res.$;
-
-	            // setup to use jquery
-	        }
-
-	        done();
-	    }
+	    callback : scrapeProductPage
 	});
 
 	// make direct hit on known url 
@@ -26,34 +16,59 @@ function scrape(uri, path = "") {
 	    uri: `${uri}/${path}`,
 	    skipEventRequest: false, // defaults to true, direct requests won't trigger Evnet:'request'
 	    callback: function(err, res) {
-	        if(err) {
-	            console.log(err)
-	        } else {
-	        	// setup to use jquery
-	            let $ = res.$;
+	    	if (err) {
+		        console.log(err)
+		    } else {
+		    	// setup to use jquery
+		        let $ = res.$;
 
-	            // store path for each product 
-	            let productPaths = [];
+		        // find all product lis on page 
+		        const products = $(".products").children();
 
-	            // find all product lis on page 
-	            const products = $(".products").children();
+		        // store individual product page urls 
+		        let productUrls = [];
 
-	            // parse href attr from each product, isolate product id
-	            for (let i = 0; i < products.length; i++) {
-	            	// get href val for each product
-	            	let productHref = $(products[i]).find('a').attr('href');
+		        // // parse href attr from each product, isolate product id
+		        for (let i = 0; i < products.length; i++) {
+		        	// get href val for each product
+		        	let productHref = $(products[i]).find('a').attr('href');
 
-	            	// add to product paths
-	            	productPaths.push(`${uri}/${productHref}`);
-	            }
+		        	// add to product paths
+		        	productUrls.push(`${uri}/${productHref}`);
+		        }
 
-	            // setup crawler queue to visit individual product pages 
-	            crawler.queue(productPaths);
-	        }
+		        // setup crawler queue to visit individual product pages 
+		        crawler.queue(productUrls);
+		    }
 	    }
 	});
 
-	
+	console.log('done');
 }
+
+function scrapeProductPage(err, res, done) {
+	if (err) {
+        console.log(err);
+    } else {
+    	// setup to use jquery
+        var $ = res.$;
+
+        // gather data 
+        const data = {
+        	title: $('.shirt-details h1').contents().not('span').text().trim(), 
+        	price: $('.price').text(),
+        	img: `${res.request.uri.host}/${$('.shirt-picture img').attr('src')}`,
+        	url: res.request.uri.href
+        };
+
+        console.log(data);
+
+        // build csv
+
+    }
+
+    done();
+}
+
 
 module.exports.scrape = scrape;
